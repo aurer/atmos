172b6751957c394789ed5e9495b6051cd9b1925a
Phil Maurer <philmau@gmail.com>
2018-10-28 19:41:30 +0000

    Remove weather lib

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const Weather = require('./lib/weather')
 const app = express()
 
 app.set('view engine', 'pug')
