876a56c35ea6e4f7992793441c4c439cd35215fe
ls <ls>
2018-10-28 20:29:41 +0000

    Remove sass script and fix typo

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
-const fs = require('fs')
 const path = require('path')
-const sensor = require('./lib/sensor')
+const Sensor = require('./lib/sensor')
 const app = express()
 
 app.set('view engine', 'pug')
@@ -9,9 +8,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
-	const sass = require('sass');
-	let result = sass.renderSync({file: './assets/scss/main.scss'});
-	fs.writeFile('./public/app.css', result.css);
 	let data = await Sensor.getValues();
 	res.render('index', {data});
 })
